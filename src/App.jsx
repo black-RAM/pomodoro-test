f6950b308e506525728099dcec890c4aa986a6b1
black-RAM <pplanes311@gmail.com>
2024-04-14 00:13:07 +0300

    attempt at session timer logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,101 @@
 import { Input, Display, Controls } from "./Content"
-
+import { useState, useEffect, useRef } from "react"
 
 function Clock() {
+  const [settings, calibrate] = useState({
+    sessionLength: 25,
+    breakLength: 5,
+  })
+
+  const [running, setRunning] = useState({
+    time: new Date(),
+    isSession: true,
+    isPaused: true,
+  })
+
+  const intervalID = useRef(null)
+
+  const sessionIncrement = () => {
+    if(settings.sessionLength < 60) {
+      calibrate(previous => {
+        return {
+          ...previous,
+          sessionLength: previous.sessionLength + 1,
+        }
+      })
+    }
+  }
+
+  const sessionDecrement = () => {
+    if(settings.sessionLength > 1) {
+      calibrate(previous => {
+        return {
+          ...previous,
+          sessionLength: previous.sessionLength - 1,
+        }
+      })
+    }
+  }
+  
+  const breakIncrement = () => {
+    if(settings.breakLength < 60) {
+      calibrate(previous => {
+        return {
+          ...previous,
+          breakLength: previous.breakLength + 1,
+        }
+      })
+    }
+  }
+  
+  const breakDecrement = () => {
+    if(settings.breakLength > 1) {
+      calibrate(previous => {
+        return {
+          ...previous,
+          breakLength: previous.breakLength - 1,
+        }
+      })
+    }
+  }
+
+  const toggleTimer = () => {
+    if(running.isPaused) {
+      intervalID.current = setInterval(() => {
+        setRunning(lastRun => {
+          return {
+            ...lastRun,
+            time: new Date(lastRun.time.getTime() - 1000)
+          }
+        })
+      }, 1000)
+    } else {
+      clearInterval(intervalID.current)
+    }
+    running.isPaused = !running.isPaused
+  }
+
+  const resetTimer = () => {
+    clearInterval(intervalID.current)
+    setRunning(lastRun => {
+      const defaultTime = new Date()
+      defaultTime.setMinutes(lastRun.isSession ? settings.sessionLength : settings.breakLength, 0)
+      return {
+        ...lastRun,
+        time: defaultTime
+      }
+    })
+  }
+
+  useEffect(resetTimer, [settings])
+
+
   return (
     <main>
-      <Input factor={"break"} value={5} />
-      <Input factor={"session"} value={25}/>
-      <Display title={"Session"} dateTime={new Date().setMinutes(25, 0)} />
-      <Controls />
+      <Input factor={"break"} value={settings.breakLength} incrementor={breakIncrement} decrementor={breakDecrement} />
+      <Input factor={"session"} value={settings.sessionLength} incrementor={sessionIncrement} decrementor={sessionDecrement} />
+      <Display title={running.isSession ? "Session" : "Break"} dateTime={running.time} />
+      <Controls toggler={toggleTimer} resetter={resetTimer} />
     </main>
   )
 }
--- src/Content.jsx
+++ src/Content.jsx
@@ -1,20 +1,22 @@
 import PropTypes from "prop-types"
 import { format } from "date-fns"
 
-const Input = ({factor, value}) => {
+const Input = ({factor, value, incrementor, decrementor}) => {
   return (
     <form>
       <h2 id={`${factor}-label`} style={{textTransform: "capitalize"}}>{`${factor} length`}</h2>
-      <button type="button" id={`${factor}-decrement`}>-</button>
+      <button type="button" id={`${factor}-decrement`} onClick={decrementor}>-</button>
       <button type="button" id={`${factor}-length`}>{`${value}`}</button>
-      <button type="button" id={`${factor}-increment`}>+</button>
+      <button type="button" id={`${factor}-increment`} onClick={incrementor}>+</button>
     </form>
   )
 }
 
 Input.propTypes = {
   factor: PropTypes.string,
-  value: PropTypes.number
+  value: PropTypes.number,
+  incrementor: PropTypes.func,
+  decrementor: PropTypes.func,
 }
 
 const Display = ({title, dateTime}) => {
@@ -31,13 +33,18 @@ Display.propTypes = {
   dateTime: PropTypes.instanceOf(Date)
 }
 
-const Controls = () => {
+const Controls = ({toggler, resetter}) => {
   return (
     <section>
-      <button type="button" id="reset">Reset</button>
-      <button type="button" id="start_stop">Start/Stop</button>
+      <button type="button" id="reset" onClick={resetter}>Reset</button>
+      <button type="button" id="start_stop" onClick={toggler}>Start/Stop</button>
     </section>
   )
+}
+
+Controls.propTypes = {
+  toggler: PropTypes.func,
+  resetter: PropTypes.func,
 }
 
 export { Input, Display, Controls }
